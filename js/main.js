b78bac54d393379dd8fdf3635b052f4263e1c9f0
Justin <zertukis@gmail.com>
2014-03-20 01:04:23 -0500

    added jquery to switch section displayed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 //make navbar translucent when not at top
 $(window).scroll(function() {   
     $(".navbar").css({opacity: 0.85});
@@ -6,3 +7,70 @@ $(window).scroll(function() {
     $(".navbar").css({opacity: 1});
     }
 });
+
+
+
+$("li .image-page").on('click', (function(){
+    $("#images").show();
+    $("#about").hide();
+    $("#myCarousel").hide();
+    $("#videos").hide();
+    $("#contact").hide();
+    $("#resume").hide();
+
+}));
+
+$("li .about-page").on('click', (function(){
+    $("#images").hide();
+    $("#about").fadeIn('slow');
+    $("#myCarousel").hide();
+    $("#videos").hide();
+    $("#contact").hide();
+    $("#resume").hide();
+
+}));
+
+$("li .home-page").on('click', (function(){
+    $("#images").hide();
+    $("#about").hide();
+    $("#myCarousel").show();
+    $("#videos").hide();
+    $("#contact").hide();
+    $("#resume").hide();
+
+}));
+
+$("li .video-page").on('click', (function(){
+    $("#images").hide();
+    $("#about").hide();
+    $("#myCarousel").hide();
+    $("#videos").show();
+    $("#contact").hide();
+    $("#resume").hide();
+
+}));
+
+$("li .contact-page").on('click', (function(){
+    $("#images").hide();
+    $("#about").hide();
+    $("#myCarousel").hide();
+    $("#videos").hide();
+    $("#contact").show();
+    $("#resume").hide();
+
+}));
+
+$("li .resume-page").on('click', (function(){
+    $("#images").hide();
+    $("#about").hide();
+    $("#myCarousel").hide();
+    $("#videos").hide();
+    $("#contact").hide();
+    $("#resume").show();
+
+}));
+
+
+
+
+
